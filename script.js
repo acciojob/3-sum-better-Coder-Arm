5b39ce6ad8b3d4b9c6390e838487e78e21d3dee3
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-06-13 10:27:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let n = arr.length; 
+	let minDiff = Number.MAX_SAFE_INTEGER;
+	let sum = 0;
+	for(let i = 0; i < n-3; i++){
+		sum = arr[i]+arr[j]+arr[k];
+		if(Math.abs(sum-target) < minDiff){
+			minDiff = Math.abs(sum-target);
+		}
+	}
+  return target + minDiff;
 }
 
 module.exports = threeSum;
